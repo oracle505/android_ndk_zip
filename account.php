0dd8baa29f1bafe633f8850485a6af13f2265fc8
oracle <oracle>
2023-08-01 04:36:13 -0700

    first commit

---
+++ account.php
@@ -0,0 +1,193 @@
+二、分卷压缩 1.使用tar分卷压缩 格式： tar zcvf - filedir | split -d -b 500m - filename 
+样例： tar zcvf - ./picture | split -d -b 500m - picture 
+将./picture打包，并切割为 500m 的包 输出的文件为 filename00、filename01、filename02 … 
+假设不加filename，则输出文件为 x00、x01、x02 … 假设不加參数 -d。则输出aa、ab、ac …
+
+ tar zcvf -  21.4.7075529 | split -d -b 128m - ndk21.4.7075529bagsplit
+tar zcvf -  21.4.7075529 | split -d -b 98m - ndk21.4.7075529bagsplit
+
+
+
+2.解压分卷 
+首先将分卷包合并 cat x* > myzip.tar.gz 
+然后解压 tar zxvf myzip.tar.gz 
+样例： cat picture* > picture.tar.gz 
+tar zxvf picture.tar.gz 
+三、error splicing file: file too large解决方法 
+FAT32格式的usb最大支持4G的文件，拷贝超过4G的文件需要把usb换成NTFS格式。
+cat ndk21.4.7075529bag* >myzip.tar.gz
+tar -zxvf myzip.tar.gz 
+
+
+后台 505 q42023 peter1232023
+账号：CD007
+密码：123456
+后台:  1. 活动按金额从小到大排序.  2. 查询功能优化.  3. 抢单和返佣功能优化  4. 后台设置抢单时间优化 5. 接口鉴权优化 an2023
+
+
+tar -czvf /data/nginx.tar.gz  /usr/local/openresty/nginx/
+
+ /www/server/php/73/bin/php think crud -t quanjushezhi  -u 1 --force=true
+ 
+  /www/server/php/73/bin/php think crud -t tkbusiness_viplevel  -u 1 --force=true 
+  
+  /www/server/php/73/bin/php think crud -t tkbusiness_viplevelmustorderrecord  -u 1 --force=true
+
+后台访问地址： JU090-6039P-08409-8J0QH-2YR7F
+
+
+https://github.com/opengapps/opengapps
+https://github.com/MoKee/android_kernel_xiaomi_surya
+https://sourceforge.net/projects/mokee/files/HISTORY/
+eco@eco123
+nd  cigWjcJgxo1O6bBM
+NIc^0Kk^Nh54$W0$zQA2cbO
+cigWjcJgxo1O6bBM2
+
+
+
+http://159.138.62./static/front_end/shift/index.html#/login
+shadow lobster barely clerk embody 
+pass 
+
+cd /www/server/panel/ && python tools.py panel test123
+2ckmqj9z https://192.168.127.128:7800/ab0d8d0e
+
+
+cd existing_repo
+
+git branch -M main
+git push -uf origin main
+
+data_/tinshop/tinshop/
+http://192.168.70.130:39015/front_end/api/frontindex2?user=2
+
+
+/www/server/php/73/bin/php think crud -t lingqu -u 1 --force=true
+
+/www/server/php/73/bin/php think crud -t lingqu -u 1 --force=true
+
+/www/server/php/73/bin/php think crud -t frontsets_superstars -u 1 --force=true 
+
+
+N4FHhNJS4hJK63rR
+前台 http://dillards25.com
+SW:ahao8888 MM:2120078cc
+
+安全碼：123
+http://dillards10.com/dillardsss 
+admin
+ahao8888
+2f33409113844a34b23d839e44151c06
+https://blog.51cto.com/u_15127513/4341921
+Jenkins--H5 front前端自动化部署（Jenkins服务在Linux系统）https://www.jenkins.io/doc/book/installing/linux/
+https://blog.csdn.net/zhishidi/article/details/118082509
+https://blog.51cto.com/u_13520761/2317390?articleABtest=0
+https://blog.csdn.net/qq15577969/article/details/123759333
+$sql = Db::table('staff')->getLastSql();
+$this->merror(__('----').$sql);
+
+Db::listen(function($sql, $time, $explain){
+    // 记录SQL
+    echo $sql. ' ['.$time.'s]';
+    // 查看性能分析结果
+    dump($explain);
+});  
+
+
+https://blog.csdn.net/sinat_14840559/article/details/108902637
+https://www.jenkins.io/doc/book/installing/linux/
+
+
+=IF(AND(LEFT(A2)="+",RIGHT(A2)="+"),REPLACE(REPLACE(A2,LEN(A2),1,""),1,1,"")
+
+
+=IF(AND(LEFT(A2)="+",RIGHT(A2)="+"),REPLACE(REPLACE(A2,LEN(A2),1,""),1,1,"")
+
+
+=IF((LEFT(B2,1)="4"),"A"&MID(B2,2,3),B2)
+
+nlaoba45 kydbb2023
+02.xyz
+Accept-Language:
+zh-CN,zh;q=0.9,km;q=0.8 
+
+
+前台 https://lsc.5ql/
+
+后台 https://lsc.5ql/admin admin@lsc  123456
+
+
+https://x.5ql/admin admin  123456
+098577210 https://1564584.vip/#/Mine
+
+
+前台 https://lsc.5ql/
+
+写真后台 https://lsc.5ql/admin admin@lsc  123456
+
+
+打后台 https://x.5ql/admin admin  123456
+
+
+客服登录入口 https://i.5ql/
+聊天后台 https://i.5ql/admin_login admin  admin888
+
+
+
+ecom   eco123@123
+https://dash.cloudflare.com/profile
+
+http://159.138:8080/view/TikTok/job/TikTok_A03_Admin_Prod_Env_2.0/   admin  eco123@123
+
+http://yk.funnyshop.org/aZRHgQIfVw.php登录域名
+
+账号：david密码123456
+
+todesk  Qwqwqw1234! 密码
+
+
+https://henhojkb.org/  
+https://www.henhojkb.org/   
+https://lsc.henhojkb.org/
+
+
+
+cun@ubuntu:~/code/vue/nsc_x_i https://www.jianshu.com/p/dca1ec8187aa
+
+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
+
+一般代理
+
+后台登入位置:https://etpay888.com/userlogin
+
+名称:MP
+
+商户号:52018
+登入帐号:mp888888
+登入密码:toby3518
+支付密码:toby3518
+ git config --global user.name "oracle"
+ git config --global user.email "oracle@pro.me"
+
+
+Link: https://billmaker.net/billTransfer/tech
+账号： 789bet
+密码： 789bet123qwe
+
+ln -sf /www/server/php/72/bin/php /usr/bin/php
+ln -sf /www/server/php/73/bin/php /usr/bin/php
+
+ln -sf /www/server/php/81/bin/php /usr/bin/php 
+
+
+
+https://bigdata.cus9/
+账号     密码
+cdadmin Cd20230727
+cd007 admin123@
+cd006 admin006@@
+cdtom admintom2023@
+
+
+
